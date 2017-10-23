65165363d0accb13cf1353f60737ed3c250b15b9
Katie Keel <akatiekeel@gmail.com>
2017-10-23 10:36:18 -0600

    Words now displaying in box, not correctly yet

--- src/submitWords.js
+++ src/submitWords.js
@@ -4,9 +4,14 @@ function submitWords() {
   $(document).ready(function() {
     $(":button").click(function(event) {
       event.preventDefault();
+      clearDisplayedWords()
       getWordsFromBox();
     })
   })
+}
+
+function clearDisplayedWords() {
+  $(".word-count").empty();
 }
 
 function getWordsFromBox() {
@@ -21,18 +26,35 @@ function parseText(text) {
 }
 
 function calculateWordFrequencies(words) {
-  let wordsWithFrequencies = {};
+  let wordsWithFrequencies = {}
   words.forEach(function(word) {
       if (!wordsWithFrequencies[word]) {
-          wordsWithFrequencies[word] = 0;
+        wordsWithFrequencies[word] = 0;
       }
       wordsWithFrequencies[word] += 1;
-  });
-  sortWordsWithFrequencies(wordsWithFrequencies)
+  })
+  sortWordsByFrequency(wordsWithFrequencies)
 }
 
-function sortWordsWithFrequencies(wordsWithFrequencies) {
-  console.log(wordsWithFrequencies);
+function sortWordsByFrequency(words) {
+  Object.keys(words).sort().forEach(function(key) {
+    let value = words[key]
+    delete words[key]
+    words[key] = value
+  })
+  displaySubmittedWords(words)
+}
+
+function clearSubmittedWords() {
+
+}
+
+function displaySubmittedWords(words) {
+  Object.keys(words).forEach(function(word) {
+    $(".word-count").append(
+      `<span font-size=${words['word']}em> ${word} </span><br/><br/>`
+    )
+  })
 }
 
 submitWords();
